91bdc0dd4e9be64a1fe4f28eb144fd16ee09b304
HunterMRocha <hmrocha2@dons.usfca.edu>
2022-03-16 20:08:13 -0700

    init commit

---
+++ utils.py
@@ -0,0 +1,16 @@
+import turtle
+
+def create_turtle(shape, color, startx=0, starty=0, dx=0, dy=0, hide="no"):
+	t = turtle.Turtle()
+	t.shape(shape)
+	t.color(color)
+	t.penup()
+	t.goto(startx, starty)
+	t.speed(0)
+	t.setheading(0)
+	t.direction = "stop"
+	t.dx = dx
+	t.dy = dy
+	if hide == "yes":
+		t.hideturtle()
+	return t
